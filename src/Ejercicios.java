791c32e1620b69ec3a2d47f0650d32aff2b08c05
JohnMazua <johnmazua@unitec.edu>
2015-02-14 20:12:46 -0600

    Update Ejercicios.java

--- src/Ejercicios.java
+++ src/Ejercicios.java
@@ -4,11 +4,12 @@ public class Ejercicios {
 		public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Rectangulo rectangulo = new Rectangulo(10,20);
-		PentagonoRegular pentagono = new PentagonoRegular(10);
+		Rectangulo rectangulo = new Rectangulo(10,20); // asigna rectangulo nuevo con dimensiones 10/20
+		PentagonoRegular pentagono = new PentagonoRegular(10); // asigna pentagono nuevo con lado 10
 		
+		// Impresion de resultados para el rectangulo y el pentagono.
 		System.out.println("El area del rectangulo es: "+rectangulo.getArea());
-		System.out.println("El perímetro del rectangulo es: "+rectangulo.getPerimetro());
+		System.out.println("El perÃ­metro del rectangulo es: "+rectangulo.getPerimetro());
 		System.out.println("");
 		System.out.println("El area de un pentagono es: "+pentagono.getPerimetro());
 		
